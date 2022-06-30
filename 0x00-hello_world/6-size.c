518da871dcaf00cd02cb74f83c39fe5175c3387b
benoni <besckinder@gmail.com>
2022-06-30 15:52:58 -0700

    Commits on c hello-world project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	long int longIntType;
 	long long int longLongIntType;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
