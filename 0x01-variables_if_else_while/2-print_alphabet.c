f701c898989311dd5dfccb1e66ebf580b6cb9dcf
benoni <besckinder@gmail.com>
2022-07-01 15:20:25 -0700

    Commits on variables and what not

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+int ch = 'a';
+
+for (ch == 'a'; ch <= 'z' ; ch++)
+	putchar(ch);
+	putchar('\n');
+
+	return 0;
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                                                                                                                              
+                                                                                                                                                
+int main(void)                                                                                                                                  
+{                                                                                                                                               
+int ch = 'a';                                                                                                                                   
+                                                                                                                                                
+for (ch == 'a'; ch <= 'z' ; ch++)                                                                                                               
+        putchar(ch);                                                                                                                            
+int CH = 'A';
+for (CH == 'A'; CH <= 'Z' ; CH++)                                                                                                               
+        putchar(CH);    
+	putchar('\n');                                                                                                                      
+                                                                                                                                                
+        return 0;                                                                                                                               
+}                    
