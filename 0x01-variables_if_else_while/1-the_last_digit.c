d902dcf36c1f9eff7f4e2b2469aa0c8d870a4e1a
benoni <besckinder@gmail.com>
2022-07-01 14:08:45 -0700

    Commits on variables and what not

---
+++ 0x01-variables_if_else_while/1-the_last_digit.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry pint
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	int lastDigit = n % 10;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %d ", n);
+		printf("is %d ", lastDigit);
+		printf("and is greater than 5\n");
+	}
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %d ", n);
+		printf("is %d ", lastDigit);
+		printf("and is 0\n");
+	}
+	else if (lastDigit < 0 && lastDigit != 0)
+	{
+		printf("Last digit of %d ", n);
+		printf("is %d ", lastDigit);
+		printf("and is less than 6 and not 0\n");
+	}
+	else
+		printf("Please try again!");
+	return (0);
+}
