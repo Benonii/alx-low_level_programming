adc384ca958b950a63671573d0a1c5be6cda3dc5
benoni <besckinder@gmail.com>
2022-07-01 16:22:17 -0700

    Commits on variables and what not

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,12 +4,19 @@ int main(void)
 {                                                                                                                                               
 int ch = 'a';                                                                                                                                   
                                                                                                                                                 
-for (; ch <= 'z' ; ch++)                                                                                                               
-        putchar(ch);                                                                                                                            
-int CH = 'A';
-for (CH == 'A'; CH <= 'Z' ; CH++)                                                                                                               
-        putchar(CH);    
-	putchar('\n');                                                                                                                      
+while (ch <= 'z')
+{
+	putchar(ch);
+	ch++;
+}
+int capCh = 'A';
+
+while (capCh <= 'z')
+{
+	putchar(capCh);
+	capCh++;
+}
+	putchar("\n");
                                                                                                                                                 
         return 0;                                                                                                                               
 }                    
