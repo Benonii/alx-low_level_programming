0abb5eb9d5d64e2cabdb4b6af62419a5c7b543c5
benoni <besckinder@gmail.com>
2022-07-01 13:28:57 -0700

    Commits on variables and what not

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ int main(void)
 
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf(n, "%d is positive");
+		printf("%d is positive", n);
 	else if (n < 0)
-		printf(n, "%d is negative");
+		printf("%d is negative", n);
 	else
-		printf(n, "%d is zero");
+		printf("%d is zero", n);
 	return (0);
 }
