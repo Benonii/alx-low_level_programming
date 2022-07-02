18134e7dc46d919d89f20fbd092c36df35f2275e
benoni <besckinder@gmail.com>
2022-07-01 17:32:42 -0700

    Commits on variables and what not

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Probably)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	return (0);
+}
