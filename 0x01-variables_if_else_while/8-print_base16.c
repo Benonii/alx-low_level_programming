3f0dc6cbc32c87c52b108cf1632fabdc6358bb5c
benoni <besckinder@gmail.com>
2022-07-02 00:54:04 -0700

    Commits on variables and what not

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	for (letter = 'A'; letter <= 'F'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar(10);
+	return (0);
+
+
+}
