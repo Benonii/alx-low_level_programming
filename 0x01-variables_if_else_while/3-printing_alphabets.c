69d2adbda3a3e591ff7e1f85f7e07ae2b98b1938
benoni <besckinder@gmail.com>
2022-07-02 01:19:21 -0700

    Commits on variables and what not

---
+++ 0x01-variables_if_else_while/3-printing_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+	int CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
+		putchar(CH);
+	}
+	putchar(10);
+	return (0);
+}
