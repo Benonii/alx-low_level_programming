6edd0b3101e00cc44b90ebf41372b8fcc2631ff0
benoni <besckinder@gmail.com>
2022-07-04 06:47:08 -0700

    Commits on variables and what not

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,14 +3,15 @@
  * main - Entry point
  *
  * Return: Always 0
- */ int main(void)
+ */
+int main(void)
 {
 	int num1;
 	int num2;
 
-	for(num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
-		for(num2 = num1 + 1 ; num2 < 10; num2++)
+		for (num2 = num1 + 1 ; num2 < 10; num2++)
 		{
 			putchar(num1 + '0');
 			putchar(num2 + '0');
