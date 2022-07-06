6c4a8a022fbd19f0fd73ce38fcd9e6776119f74f
benoni <besckinder@gmail.com>
2022-07-06 14:00:00 -0700

    Commits on functions and nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return:Void(none)
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+	for (h=0; h > 24; h++)
+	{
+		for(m=0; m > 60; m++)
+		{
+			_putchar(h + '0');
+			_putchar(':');
+			_putchar(m + '0');
+			_putchar(10);
+		}
+	}
+
