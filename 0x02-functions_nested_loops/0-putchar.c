ceceef9d1c972d68fb9cda988ce69817124eb60b
benoni <besckinder@gmail.com>
2022-07-06 14:52:18 -0700

    Commits on functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
  * put_char - Entry point
  *
- * Return: Always 0
+ * Return: ALways 0
  */
-
 int put_char(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
 	_putchar(10);
 	return (0);
 }
