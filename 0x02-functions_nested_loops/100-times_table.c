73e3c8cce7ba260061568b0b7c244fd25c859097
benoni <besckinder@gmail.com>
2022-07-06 16:01:35 -0700

    Commits on functions and nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_time_tables - Entry point
+ * print_times_tables - Entry point
  *
  * @n: The value of the times table
  */
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int num, mult, prod;
 
-	if (n >= 0 && n <=15)
+	if (n >= 0 && n <= 15)
 	{
 		for (num = 0; num <= n; num++)
 		{
@@ -30,7 +30,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) + '0');
+					_putchar(((prod / 10) % 10) + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
