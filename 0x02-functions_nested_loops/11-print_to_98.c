540c131b92b9a2c0f0184b797c5332eed33f1e1c
benoni <besckinder@gmail.com>
2022-07-06 14:31:01 -0700

    Commits on functions and nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: Starting point
+ * Return: None(void)
+ */
+
+void print_to_98(int n);
+{
+	for (n; n <= 98; n++)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+	putchar(10);
+}
