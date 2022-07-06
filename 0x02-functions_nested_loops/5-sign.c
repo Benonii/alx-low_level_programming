2f0737bff97b9d22d0ef78b4f7229e97dd847964
benoni <besckinder@gmail.com>
2022-07-06 12:39:08 -0700

    Commits on functions and nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
