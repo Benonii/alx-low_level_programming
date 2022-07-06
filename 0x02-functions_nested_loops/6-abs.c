e143b3c75658cbb6ab824e8965b1482f4ca9c758
benoni <besckinder@gmail.com>
2022-07-06 13:07:00 -0700

    Commits on functions and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+		_putchar((n * -1) + '0');
+	else
+		_putchar(n + '0');
+	return (0);
