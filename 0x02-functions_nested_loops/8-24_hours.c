8040e10c73f9eb0db273889410008b11cdac7b74
benoni <besckinder@gmail.com>
2022-07-06 14:04:29 -0700

    Commits on functions and nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,9 +14,11 @@ void jack_bauer(void)
 	{
 		for(m=0; m > 60; m++)
 		{
-			_putchar(h + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar(m + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar(10);
 		}
 	}
