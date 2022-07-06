0b85adcaa1fed6f8e67f3618ace574a912ab31c1
benoni <besckinder@gmail.com>
2022-07-06 13:19:56 -0700

    Commits on functions and nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,13 @@
 /**
  * _abs - Entry point
  *
+ * @n: Any integer in this context
  * Return: Always 0
  */
 int _abs(int n)
 {
 	if (n >= 0)
-		return(n);
+		return (n);
 	else
-		return(-n);
+		return (-n);
 }
