42b5fbbe684f18c4759ed8110fe8c796549dbfc2
benoni <besckinder@gmail.com>
2022-07-07 07:26:51 -0700

    Commits on more functions.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - Identifies uppercaase letters
+ * @c: The alphabet holder variable
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c < 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+"This is a readme file for more functions. This should be fun."
+
