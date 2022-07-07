96db74a969206b5f5323b2fc6afd6a6d260f7c6e
benoni <besckinder@gmail.com>
2022-07-07 12:20:59 -0700

    Commits on more functions.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers from 0 to 10
+ * more_numbers - prints numbers from 0 to 14 10 times
  *
  * Return: Void
  */
@@ -14,6 +14,8 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
+			if (num > 9)
+				_putchar((num / 10) + '0')
 			_putchar(num + '0');
 		}
 	}
