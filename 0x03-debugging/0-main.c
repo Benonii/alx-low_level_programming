35d0f590641de0db0d78125dd4c9aecf429e09de
benoni <besckinder@gmail.com>
2022-07-07 04:08:29 -0700

    Commits on debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include m"main.h"
+/**
+ *
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	
+	i = 98;
+	if (i == 0)
+		printf( "%d is zero", i);
+	positive_or_negative(i);
+
+	
