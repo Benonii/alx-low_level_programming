dbec6f9b6d79ad9aa36411ee9d057908cd673a22
benoni <besckinder@gmail.com>
2022-07-07 12:03:35 -0700

    Commits on more functions.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 10 except 2 and 4
+ *
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if ( num == 2 || num == 4)
+			continue;
+		_putchar(num + '0');
+	}
+	_putchar(10);
+}
