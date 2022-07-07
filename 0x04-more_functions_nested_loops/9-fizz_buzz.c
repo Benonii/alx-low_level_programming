c04437953660181bae5dcab2ab78b19a0a895973
benoni <besckinder@gmail.com>
2022-07-07 14:53:35 -0700

    Commits on more functions.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,18 +13,18 @@ int main(void)
 
 	for (i = 0; i <= 100; i++)
 	{
-		if (i % 3)
-			printf("Fizz");
-		else if (i % 5)
-			print("Buzz");
-		else if (i % 3 && i % 5)
+		if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz");
+		else if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
 		else
 			printf("%d", i);
 
 		if (i == 100)
 			continue;
-		printf(" ")
+		printf(" ");
 	}
 	printf("\n");
 
