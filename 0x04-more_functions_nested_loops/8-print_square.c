00076b33b183d0258bb28f038d9cb987f31763a1
benoni <besckinder@gmail.com>
2022-07-07 14:22:04 -0700

    Commits on more functions.

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,38 +1,22 @@
 #include "main.h"
 /**
- * print_square - Prints a square on the terminal
- * @size: Determines how big the square will be
+ * print_square - Prints a square
+ * @size: Size of the square
  */
 void print_square(int size)
 {
-	int length, space, width;
-
-	for (width = 0; width < size; width++)
-	{
-		_putchar('_');
-	}
-
-	putchar(10);
+	int length, width;
 
 	for (length = 0; length < size; length++)
 	{
-		_putchar('|');
-
-		for (space = 0; space < width; width++)
+		for (width = 0; width < size; width++)
 		{
-			_putchar(' ');
+			_putchar("#");
 
-			if (space == width - 2)
-				break;
-		}
-		_putchar('|');
+		if (length == n - 1)
+			continue;
 		_putchar(10);
-	}
-
-	for (width = 0; width < size; width++)
-	{
-		_putchar("_")
+		}
 	}
 	_putchar(10);
 }
-
---
+++ 0x04-more_functions_nested_loops/my_own_square.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_square - Prints a square on the terminal
+ * @size: Determines how big the square will be
+ */
+void print_square(int size)
+{
+	int length, space, width;
+
+	for (width = 0; width < size; width++)
+	{
+		_putchar('_');
+	}
+
+	putchar(10);
+
+	for (length = 0; length < size; length++)
+	{
+		_putchar('|');
+
+		for (space = 0; space < width; width++)
+		{
+			_putchar(' ');
+
+			if (space == width - 2)
+				break;
+		}
+		_putchar('|');
+		_putchar(10);
+	}
+
+	for (width = 0; width < size; width++)
+	{
+		_putchar("_");
+	}
+	_putchar(10);
+}
+
