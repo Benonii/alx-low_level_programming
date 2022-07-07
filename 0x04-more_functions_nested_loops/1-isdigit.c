c4c4a9895e6e32a2a0740abd49264bbf0499522a
benoni <besckinder@gmail.com>
2022-07-07 09:38:18 -0700

    Commits on more functions.

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - Identifies uppercaase letters
+ * @c: The alphabet holder variable
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
