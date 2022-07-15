73679f0cc8ea352d42ae11395fef1c51ce4e4f02
benoni <besckinder@gmail.com>
2022-07-15 03:27:11 -0700

    Commits on pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,9 @@
 /**
  * _strlen - returns the length of the string
  * @s: the pointer to the string
+ * Return: the size of the string
  */
 int _strlen(char *s)
 {
-	return (sizeof(*s));
+	return (sizeof(&s));
 }
