11b7b118301c4451beec4f751649d1ab8ec8f9e1
benoni <besckinder@gmail.com>
2022-07-15 04:12:26 -0700

    Commits on pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - Prints a given string in reverse
+ * @s: the pointer to the string
+ */
+
+void print_rev(char *s)
+{
+	int len = 0, index;
+
+	while (s[index++])
+		len++;
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+	_putchar(10);
+}
