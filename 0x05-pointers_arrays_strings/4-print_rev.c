96517dcdc5aa0c27b47b4f6d4bd331c081fe9a0a
benoni <besckinder@gmail.com>
2022-07-15 04:30:41 -0700

    Commits on pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 
 	while (s[index++])
 		len++;
-	for (index = len - 1; index >= 0; index--)
+	for (index = len; index >= 0; index--)
 		_putchar(s[index]);
 	_putchar(10);
 }
