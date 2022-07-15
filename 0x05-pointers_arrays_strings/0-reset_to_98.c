0b6f2ae2cf8fc80751f432589487f926231e6e46
benoni <besckinder@gmail.com>
2022-07-15 03:07:06 -0700

    Commits on pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * return_to_98 - Changes the variable stored at the address to 98
+ * @n: the pointer
+ */
+void return_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+"this is a readme file on pointers, arrays and strings.
+It includes the answers to  9 mandatory tasks and possibly 2 advanced tasks."
+"Enjoy!"
