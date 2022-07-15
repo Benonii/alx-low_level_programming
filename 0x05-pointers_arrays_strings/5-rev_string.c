e9034ba46227407f87b6eec45bd47acbc1651f94
benoni <besckinder@gmail.com>
2022-07-15 04:29:34 -0700

    Commits on pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: The string to be reversed
+ */
+void rev_string(char *s)
+{
+	int len = 0, index;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		*s = s[index];
+}
