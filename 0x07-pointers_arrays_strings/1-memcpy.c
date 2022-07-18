5a8294c61e565e42e3a3bd0a555c9555357da4dc
benoni <besckinder@gmail.com>
2022-07-18 10:27:59 -0700

    Commits on even more pointers, arrays and strings.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies bytes from src to dest
+ * @src: address to be copied from
+ * @dest: address to be copied to
+ * @n: the number of bytes to be copied
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+	char **ddest;
+
+	for (index = 0; index < n; index++)
+		*dest = src[index];
+	*ddest = dest;
+
+	return (*ddest);
+}
