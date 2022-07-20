2083c1a12a05773b7d984c48b1ca868f7aba3574
benoni <besckinder@gmail.com>
2022-07-20 09:19:53 -0700

    Commit messages on recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * prime - checks weather its prime
+ * @m: possible factors
+ * @num: number being tested
+ * Return: 1 or 0
+ */
+int prime(int m, int num)
+{
+	if (num % m == 0)
+		return (0);
+	if (m == num ** 1/2)
+		return (1);
+	return (prime(m+1, num));
+}
+/**
+ * _is_prime_number - checks prime or not
+ * @n - number
+ * Return: 1 0r 0
+ */
+int _is_prime_number(int n)
+{
+	int m = 0;
+	return(prime(n, m));
+}
