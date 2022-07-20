d09584c96b770a2dd5532d9774dd180d6a0bf8bb
benoni <besckinder@gmail.com>
2022-07-20 06:39:42 -0700

    Commit messages on recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,8 +10,8 @@ int _strlen_recursion(char *s)
 
 	if (*s)
 	{
-		_strlen_recursion(s + 1);
-		length = strlen(s);
+		length++;
+		length += _strlen_recursion(s + 1);
 	}
-	return (strlen);
+	return (length);
 }
