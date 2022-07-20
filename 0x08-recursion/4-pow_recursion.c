7d42f3dbb5af2330549b21df87631be224044f2e
benoni <besckinder@gmail.com>
2022-07-20 07:14:23 -0700

    Commit messages on recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,9 +7,12 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
+	if (y < 0)
 		return (-1);
+	else if (y == 0)
+		return (1);
 	else if (y == 1)
 		return (x * 1);
+
 	return (x * _pow_recursion(x, y - 1));
 }
