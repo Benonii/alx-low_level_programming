410efc03dc910411532c033639f82b0bebb81fb8
benoni <besckinder@gmail.com>
2022-07-20 06:49:25 -0700

    Commit messages on recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a number
+ * @n: The number
+ * Return: The factorial
+ */
+int factorial(int n);
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+	{
+		return (n * factorial(n-1));
+	}
+}
