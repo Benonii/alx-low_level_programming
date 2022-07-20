250b2a7d0db97a5ae8b38663b7e3b198bb261ae0
benoni <besckinder@gmail.com>
2022-07-20 07:12:22 -0700

    Commit messages on recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - return x the power of y
+ * @x: The base
+ * @y: The exponent
+ * Return: The power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (-1);
+	else if (y == 1)
+		return (x * 1);
+	return (x * _pow_recursion(x, y - 1));
+}
