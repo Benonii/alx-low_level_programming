33b62b6581d345e34674f77561243f78c37b0712
benoni <besckinder@gmail.com>
2022-07-21 04:21:51 -0700

    Commits on pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - prints every other string starting from the first one
+ * @str: the string that we will go through
+ */
+void puts2(char *str)
+{
+	int index = 0;
+
+	while (str[index++])
+	{
+		_putchar(str[index]);
+		index++;
+	}
+}
