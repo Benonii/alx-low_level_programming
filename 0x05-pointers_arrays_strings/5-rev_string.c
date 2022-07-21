d93ce96ead4aa1d11b315040f8a6a34ab8f96dfc
benoni <besckinder@gmail.com>
2022-07-21 04:05:01 -0700

    Commits on pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,11 +5,16 @@
  */
 void rev_string(char *s)
 {
-	int len = 0, index = 1;
+	int len = 0, index = 0;
+	char revedWord;
 
 	while (s[index++])
 		len++;
 
 	for (index = len - 1; index >= 0; index--)
-		*s = s[index];
+	{
+		revedWord = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = revedWord;
+	}
 }
