1ecf6cc4e3e5d7c117b2da900e2c910dc595b514
benoni <besckinder@gmail.com>
2022-07-21 05:54:21 -0700

    Commits on pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,16 +6,19 @@
 
 void puts_half(char *str)
 {
-	int index = 0, len = 0;
+	int index = 0, len = 0, n;
 
 	while (str[index++])
 		len++;
 
-	for (index = len / 2; index < len; index++)
-	{
-		if (len % 2 != 0)
-			_putchar(str[index + 1]);
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
 		_putchar(str[index]);
-	}
+
 	_putchar(10);
 }
