5e1eb2723d328f6bd94c997936b622ec22f0da2c
benoni <besckinder@gmail.com>
2022-07-21 05:03:03 -0700

    Commits on pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array
+ * @a: The array
+ * @n: The number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int index = 0, len = 0;
+
+	while (a[index++])
+		len++;
+
+	for (index = 0; index < n && index < len; index++)
+	{
+		_putchar(a[index]);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar(10);
+}
