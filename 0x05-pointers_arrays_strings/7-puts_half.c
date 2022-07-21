7b0feadd49f1c6462e45b05bcf3ab3a6e7e207d7
benoni <besckinder@gmail.com>
2022-07-21 04:49:56 -0700

    Commits on pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - Prints the second half of a string
+ * @str: The string
+ */
+
+void puts_half(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = len / 2; index <= len; index++)
+	{
+		if (len % 2 != 0)
+			_puthcar(s[index + 1]);
+		_putchar(s[index]);
+	}
+	_putchar(10);
+}
