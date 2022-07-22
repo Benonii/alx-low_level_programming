b1a641d2a3a5a9cbdd06541151cadbec9e644411
benoni <besckinder@gmail.com>
2022-07-22 16:16:22 -0700

    Commits on pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#includen"main.h"
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: Destination
+ * @src: source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char src = sourceString, char dest = destString;
+	int index - 0, len = 0;
+
+	while (s[index++])
+		len++;
+
+	for (index = 0; index < len; index++)
+		sourceString += str[index];
+	destString = sourceString;
+	
+	return(dest);
+}
