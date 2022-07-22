2043ecc2db752fb83ec5371b7a7405c9ac6ab7fe
benoni <besckinder@gmail.com>
2022-07-22 16:21:26 -0700

    Commits on pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,14 +6,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char src = sourceString, dest = destString;
 	int index, len = 0;
 
 	while (s[index++])
 		len++;
 
 	for (index = 0; index < len; index++)
-		sourceString += str[index];
+		dest[index] = src[index];
+
 	destString = sourceString;
 	
 	return(dest);
