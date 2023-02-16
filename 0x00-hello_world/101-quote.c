f96eb1e72e1de54616704460faafba0fca48232c
root <besckinder@gmail.com>
2023-02-16 02:06:29 -0800

    Commits on advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpur, 2015-19-19,\n");
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpur, 2015-19-19\n");
 
 	return (1);
 }
