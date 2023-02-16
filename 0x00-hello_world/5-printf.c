c7ac1c95e4c11f7d44db1060aaec3593fa3caf6c
root <besckinder@gmail.com>
2023-02-16 01:15:00 -0800

    Fixing errors for tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art\n");
 
-	return 0;
+	return (0);
 }
