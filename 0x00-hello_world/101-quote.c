17c2529c9c981d21c15fd3547ce2c958b4c726be
root <besckinder@gmail.com>
2023-02-16 02:01:45 -0800

    Commits on advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpur, 2015-19-19,\n");
+
+	return (1);
+}
