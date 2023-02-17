e7af7e6ac38585dfe02970f380b7cc9d4b0d7e74
root <besckinder@gmail.com>
2023-02-17 15:06:34 -0800

    commits on advanced tasks

---
+++ 0x01-variables_if_else_while/100-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if (j <= i)
+				continue;
+
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 8 && j == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(10);
+	return(0);
+}
