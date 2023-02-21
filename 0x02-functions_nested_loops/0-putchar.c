5cf6faa044ae715eaa80dec5f163b20a60db4a7f
root <besckinder@gmail.com>
2023-02-21 01:34:05 -0800

    Commit messages on functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - the main function
@@ -8,13 +9,14 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
+	putchar('_');
+	putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar(10);
 	return (0);
 }
