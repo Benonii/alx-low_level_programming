8905993143e5c379e6610a291cac07de56c43d40
root <besckinder@gmail.com>
2023-02-21 13:07:15 -0800

    Fixing errors

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,8 +8,8 @@
  * Return: sum
  */
 
-int add(int a, int b, int sum)
+int add(int a, int b)
 {
-	sum = a + b;
+	int sum = a + b;
 	return (sum);
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,8 @@ void print_to_98(int n)
 {
 	for (; n < 99; n++)
 	{
-		_putchar(n);
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
 		if (n == 98)
 			break;
 		_putchar(',');
