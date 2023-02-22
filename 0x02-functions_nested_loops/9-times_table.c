bdec74ce11a9c1055a9c841cfa61987f016b2bcc
root <besckinder@gmail.com>
2023-02-22 03:03:20 -0800

    Fixing errors

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar((x % 10) + '0');
 			}
 
-			if (r == 9)
+			if (r == 9 && c != 9)
 				_putchar(10);
 
 			else
@@ -31,10 +31,6 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			_putchar(',');
-			_putchar(' ');
 		}
-		_putchar(10);
 	}
-	_putchar(10);
 }
