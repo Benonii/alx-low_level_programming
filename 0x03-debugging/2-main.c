0c8f706a84960bfac22deb3061a26de425da4d22
root <besckinder@gmail.com>
2023-02-23 01:04:13 -0800

    Commits on the debugging porject

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b && a > c)
+    {
+        largest = a;
+    }
+    else if (b > a && b > c)
+    {
+        largest = b;
+    }
+    else if (c > a && c > b)
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
+
+int main(void)
+{
+        int a, b, c;
+        int largest;
+
+        a = 972;
+        b = -98;
+        c = 0;
+
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
+}
