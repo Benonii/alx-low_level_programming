a570fe334cd753b36ed1bef27cd4d02b53792772
root <besckinder@gmail.com>
2023-02-23 12:44:06 -0800

    Fixing errors

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,11 +11,13 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size <= 0)
+		_putchar(10);
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 			_putchar('#');
 		_putchar(10);
 	}
-	_putchar(10);
 }
