b7dda1c33af5910037c702ec9e55247f637efe45
root <besckinder@gmail.com>
2023-02-23 12:35:18 -0800

    Fixing errors

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -11,11 +11,12 @@ void print_diagonal(int n)
                 putchar(92);
 	putchar(10);
         }
-
+	putchar(10);
 }
 
 int main(void)
 {
-	print_diagonal(10);
+	print_diagonal(0);
+	print_diagonal(-4);
 	return (0);
 }
