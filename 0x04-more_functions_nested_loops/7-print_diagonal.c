41ecb9d612857acceb4d9561de26badd3f576355
root <besckinder@gmail.com>
2023-02-23 12:44:06 -0800

    Fixing errors

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,6 +11,9 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+		_putchar(10);
+
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j <= i; j++)
@@ -19,5 +22,4 @@ void print_diagonal(int n)
 		_putchar(92);
 		_putchar(10);
 	}
-	_putchar(10);
 }
