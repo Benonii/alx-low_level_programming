47c88a812526db79a25bbac838c8a603deb2f1cd
root <besckinder@gmail.com>
2023-02-24 10:35:07 -0800

    Commits on advanced tasks

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints a 9x9 time table
+ * @n: the limit of the times table
+ *
+ * Return: none
+ */
+
+void print_times_table(int n)
+{
+	int i, m, p;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('0');
+
+		for (m = 1; m <= n; m++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			p = i * m;
+
+			if (p <= 9)
+				_putchar(' ');
+			else
+				_putchar((p / 10) + '0');
+
+			_putchar((p % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
