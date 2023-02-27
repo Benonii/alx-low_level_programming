896cf29c0a06c66522cf1bb232dea6a1fb98b490
root <besckinder@gmail.com>
2023-02-27 12:56:29 -0800

    Fixing errors

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,17 +19,16 @@ void puts_half(char *str)
 		i++;
 	}
 
-	n = ((count - 1) / 2);
 
 	if (count % 2 == 0)
-	{
-		for (i = (count / 2); i < count; i++)
-			_putchar(str[i]);
-	}
+
+		n = count / 2;
+
 	else
-	{
-		for (i = n; i < count; i++)
-			_putchar(str[i]);
-	}
+		n = ((count - 1) / 2);
+
+	for (i = n; i < count; i++)
+		_putchar(str[i]);
+
 	_putchar(10);
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,7 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
 	while (src[i])
 	{
