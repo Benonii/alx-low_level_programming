09b89d5ffb82db729c6de232d15205d5442dcbd4
root <besckinder@gmail.com>
2023-02-27 12:25:31 -0800

    Fixing errors

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -7,42 +7,27 @@
  * Return: Always 0.
  */
 
-void print_rev(char *s)
+void _puts(char *str)
 {
         int i = 0;
         int count = 0;
-        int j;
 
-        while (s[i] != 0)
+        while (str[i] != 0)
         {
                 count++;
                 i++;
         }
 
-        for (i = 0; i < count; i++)
-        {
-                j = count - i;
-
-                if ( s[j] == '\0')
-                        continue;
-
-                s[i]  = s[j];
-        }
         for (i = 0; i <= count; i++)
-        {
-                if (i == count)
-                        s[i] = '\0';
-
-                putchar(s[i]);
-        }
-        putchar(10);
+                _putchar(str[i]);
+        _putchar(10);
 }
 
 int main(void)
 {
-	char *str;
+    char *str;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
-	
-	return (0);}
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
