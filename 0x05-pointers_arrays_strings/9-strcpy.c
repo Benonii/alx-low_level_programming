3baaa2c5ca0956fd0dc834025f642281dda79c8c
root <besckinder@gmail.com>
2023-02-28 01:04:38 -0800

    Fixing errors

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,19 +8,15 @@
  * Return: pointer to dest
  */
 
-char *_strcpy(char *dest,  char *src)
+char *_strcpy(char *dest, const  char *src)
 {
 	int i = 0;
-	int count = 0;
 
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (dest[i++])
-		count++;
-	dest[count + 1] = '0';
 
 	return (dest);
 }
