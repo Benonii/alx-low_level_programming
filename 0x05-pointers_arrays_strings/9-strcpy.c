0910e629c1bba651e431e13896ae699dc9056d9e
root <besckinder@gmail.com>
2023-02-28 01:01:13 -0800

    Fixing errors

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,16 @@
 char *_strcpy(char *dest,  char *src)
 {
 	int i = 0;
+	int count = 0;
 
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	while (dest[i++])
+		count++;
+	*dest[count + 1] = '0';
+
 	return (dest);
 }
