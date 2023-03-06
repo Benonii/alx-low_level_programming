7e152418361db8ba0f0258a5ccc3801e494a8a3d
root <besckinder@gmail.com>
2023-03-06 08:32:27 -0800

    Fixing errors

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,9 @@
 
 char *_strchr(char *s, char c)
 {
-	int i, count;
+	int i;
 
-	while (s[i++])
-		count++;
-
-	for (i = 0; i < count; i++)
+	for (i = 0; i >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
