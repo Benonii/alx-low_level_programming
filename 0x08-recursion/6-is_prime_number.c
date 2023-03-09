b834ce4fb2a42631d88e7db4b35864674c50c2ee
root <besckinder@gmail.com>
2023-03-09 13:31:31 -0800

    First try

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,26 +10,28 @@
 
 int check_if_prime(int num, int div)
 {
+	if (num % div == 0)
+		return (0);
+
 	if (div == num / 2)
 		return (1);
 
-	if (num % div == 0)
-		return (0);
-	
-	check_if_prime(num, div + 1);
+	return (check_if_prime(num, div + 1));
 }
 
 /**
- * int is_prime_number - returns the result of previous function
+ * is_prime_number - returns the result of previous function
  * @n: Nuber to be checked
  *
  * Return: check_if_prime
- * .
+ *
  */
 
 int is_prime_number(int n)
 {
-	if (n <= 0)
+	int div = 2;
+
+	if (n <= 1)
 		return (0);
 
 	return (check_if_prime(n, div));
