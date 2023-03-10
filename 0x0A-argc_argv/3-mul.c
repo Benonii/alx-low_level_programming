413d1c1778c6d1e79241adfb32caaefbb3b04349
root <besckinder@gmail.com>
2023-03-10 13:00:44 -0800

    Commit on argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,19 +12,17 @@ int main(int argc, char *argv[])
 {
 	int mul, n, m;
 
-	if (argc == 3)
+	if (argc != 3)
 	{
-		n = atoi(argv[1]);
-		m = atoi(argv[2]);
-
-		mul = n * m;
-		printf("%d\n", mul);
-
-		exit(EXIT_SUCCESS);
-	}
-	else
-	{
-		printf("ERROR");
+		printf("Error\n");
 		exit(EXIT_FAILURE);
 	}
+
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+
+	mul = n * m;
+	printf("%d\n", mul);
+
+	exit(EXIT_SUCCESS);
 }
