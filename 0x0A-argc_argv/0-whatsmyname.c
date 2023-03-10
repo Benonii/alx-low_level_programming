a9cd96231f614e2efeebfb20382feaf8e02245bd
root <besckinder@gmail.com>
2023-03-10 12:11:35 -0800

    Commit on argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,10 @@
 
 int main (int argc, __attribute__ ((unused)) char *argv[])
 {
-	int count;
-	
-	while (argc++)
-		count++;
-	printf("%d", count);
+
+	(void) argc;
+
+	printf("%s\n", argv[0]);
 
 	exit(EXIT_SUCCESS);
 }
