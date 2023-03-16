b055f5ef4f9f14cf100c91fc250b9b44d4faba20
root <besckinder@gmail.com>
2023-03-16 09:01:34 -0700

    First try

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,19 +27,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (constr == NULL)
 		return (NULL);
 
-	for (i = 0; s2[i]; i++)
-		count++;
-
-	if (n >= count)
-	{
-		for (j = 0; j < count; j++)
-			constr[j] = s2[j];
-	}
-
 	for (i = 0; s1[i]; i++)
 		constr[i] = s1[i];
 
-	for (j = 0; j < n; j++)
+	for (j = 0; s2[i] && j < n; j++)
 		constr[i++] = s2[j];
 
 	return (constr);
