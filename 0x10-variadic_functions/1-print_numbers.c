431a9cc09e01fe8eed86637872721b5a13eb8d00
root <besckinder@gmail.com>
2023-03-23 12:22:30 -0700

    Fixed print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,14 +10,13 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int num = 0;
 	unsigned int i;
 	va_list args;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(nums, int));
+		printf("%d", va_arg(args, int));
 
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
