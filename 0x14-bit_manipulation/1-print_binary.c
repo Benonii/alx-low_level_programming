039f3340eb7b01e66a2ef74f0aeb5dd2d7708556
BENONI ESCKINDER <besckinder@gmail.com>
2023-04-07 19:23:38 +0300

    Pushing main tasks

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <sys/types.h>
+
+/**
+ * print_binary - prints the binary represantation of an int
+ * @n: The unsigned int
+ * Return: None
+ */
+
+void print_binary(unsigned long int n)
+{
+    if (n > 1)
+        print_binary(n >> 1);
+
+    putchar((n & 1) + '0');
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of flips necesarry to equate two ints
+ * @n: int 1
+ * @m: int 2
+ * Return: The counts
+ */
+
+unsigned int flip_bits(unsigned long int n, long int m)
+{
+    unsigned int count = 0;
+    int diffs = n ^ m;
+
+    while (diffs >= 1)
+    {
+        count += (diffs & 1);
+        diffs >>= 1;
+    }
+
+    return (count);
+}
