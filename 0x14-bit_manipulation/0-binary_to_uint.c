c1b33aaeb27e10faf05f863339122c6593840b83
root <besckinder@gmail.com>
2023-04-07 09:36:44 -0700

    Added edge case

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i] == '0' || b[i] == '1'; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-			return (0);
+			continue;
 		n <<= 1;
 		n += b[i] - '0';
 	}
