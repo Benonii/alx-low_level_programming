44842f5d0c02831ee4718efbd7195d36f64b42bf
root <besckinder@gmail.com>
2023-04-10 13:29:35 -0700

    Ficxed Betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,21 +9,19 @@
 
 int main(int argc, char **argv)
 {
-    int o1, o2, r, w2;
-    const char *file_from, *file_to;
-    file_from = argv[1];
-    file_to = argv[2];
+	int o1, o2, r, w2;
+	const char *file_from, *file_to;
 
-    if (argc != 3)
-        exit (97);
-    o1 = open(file_from, O_RDONLY);
-    o2 = open(file_to, O_CREAT | O_WRONLY);
+	file_from = argv[1];
+	file_to = argv[2];
 
-    if (file_from == NULL || file_to == NULL)
-        return (-1);
+	if (argc != 3)
+		exit(97);
+	o1 = open(file_from, O_RDONLY);
+	o2 = open(file_to, O_CREAT | O_WRONLY);
 
+	if (file_from == NULL || file_to == NULL)
+		return (-1);
 
-
-
-    return (0);
+	return (0);
 }
