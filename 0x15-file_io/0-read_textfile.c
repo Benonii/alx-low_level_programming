379e49cda14b0d553654acce8a32c84d47154bbb
BENONI ESCKINDER <besckinder@gmail.com>
2023-04-10 23:28:45 +0300

    Commiting from personal IDE

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: Points to the file to be read_textfile
+ * @letters: Number of letters it should read and print
+ * Return: The number of letters read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+    FILE *f;
+    ssize_t n, m, o;
+
+    f = malloc(sizeof(char) * letters);
+
+    if (f == NULL || filename == NULL)
+        return (0);
+
+    o = open(filename, O_RDONLY);
+    m = read(o, f, letters);
+    n = write(STDOUT_FILENO, f, m);
+
+    if (o == -1 || m == -1 || n == -1 || m != n) /* Checks if open, read, write execited properly*/
+        return (-1);
+
+    free(f);
+    return (n);
+}
