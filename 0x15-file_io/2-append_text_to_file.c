d3ca403576d7c0dfff9e08d2ef46369c1daa0b03
BENONI ESCKINDER <besckinder@gmail.com>
2023-05-08 19:20:12 +0300

    Commiting from personal IDE

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: The file to append to
+ * @text_content: The text to be appended
+ * Return: 1 if successful, -1 if failed.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int o, w;
+
+    if (filename == NULL)
+        return (-1);
+
+    o = open(filename, O_WRONLY | O_APPEND);
+    w = write(o, text_content, strlen(text_content));
+
+    if (o == -1 || w == -1)
+        return (-1);
+
+    return (1);
+}
