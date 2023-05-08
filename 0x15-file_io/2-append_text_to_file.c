4216e232362405525ce6245993a7c291df82a9aa
root <besckinder@gmail.com>
2023-05-08 12:03:19 -0700

    Fixed Betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,16 +9,16 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int o, w;
+	int o, w;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    o = open(filename, O_WRONLY | O_APPEND);
-    w = write(o, text_content, strlen(text_content));
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, strlen(text_content));
 
-    if (o == -1 || w == -1)
-        return (-1);
+	if (o == -1 || w == -1)
+		return (-1);
 
-    return (1);
+	return (1);
 }
