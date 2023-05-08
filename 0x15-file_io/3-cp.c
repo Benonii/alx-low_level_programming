119cb1efb3090940deeac968100cd5f33f2029e9
root <besckinder@gmail.com>
2023-05-08 14:11:44 -0700

    Fixed the name

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,28 +2,69 @@
 
 /**
  * main - Entry point
- * @argc: Number of arguments passed to main
- * @argv: array of pointers to the arguments
+ * @argc: Argument count
+ * @argv: Arguments
  * Return: Always 0
  */
 
-int main(int argc, char **argv)
+int main (int ac, char **av)
 {
-    int o1, o2, r, w2;
-    const char *file_from, *file_to;
-    file_from = argv[1];
-    file_to = argv[2];
+	int res;
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-    if (argc != 3)
-        exit (97);
-    o1 = open(file_from, O_RDONLY);
-    o2 = open(file_to, O_CREAT | O_WRONLY);
+	if (av[1] == NULL)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	
+	if (av[2] == NULL)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 
-    if (file_from == NULL || file_to == NULL)
-        return (-1);
+	res = cp_content(av[1], av[2]);
+	printf("%d", res);
 
+	return (0);
+}
 
+int cp_content(char *file_from, char *file_to)
+{
+	char *buf;
+	int o, op, r, w, c, cl;
 
+	buf = malloc(1024);
+	o = open(file_from, O_RDONLY);
+	op = open(file_to, O_CREAT | O_RDWR | O_TRUNC, 0664);
+	r = read (o, buf, 10000);
+	w = write(op, buf, r);
 
-    return (0);
+	free(buf);
+	c = close(o);
+	cl = close(op);
+
+	if (o == -1 || op == -1 || r == -1 || w == -1 || r != w)
+	{
+		return (-1);
+	}
+	
+	if (c == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", c);
+		exit(100);
+	}
+
+	if (cl == -1)
+        {
+                dprintf(2, "Error: Can't close fd %d", cl);
+                exit(100);
+        }
+
+	return (1);
 }
