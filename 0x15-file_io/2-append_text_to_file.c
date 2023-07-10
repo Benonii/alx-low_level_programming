2374b88a87454c1d348edb2c00c613e0dd927a2a
root <besckinder@gmail.com>
2023-07-10 12:19:39 -0700

    Fixed indentation issues

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,22 +3,22 @@
 /**
  * append_text_to_file - appends text to the end of a file
  * @filename: The file to append to
- * @text_content: The text to be appended
- * Return: 1 if successful, -1 if failed.
+ * @text_content: The text to be apeended
+ * Return: 1 if succesful, -1 if failed
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int o, w;
+	int o, w;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    o = open(filename, O_WRONLY | O_APPEND);
-    w = write(o, text_content, strlen(text_content));
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, strlen(text_content));
 
-    if (o == -1 || w == -1)
-        return (-1);
+	if (o == -1 || w == -1)
+		return (-1);
 
-    return (1);
+	return (1);
 }
