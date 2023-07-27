fbcea45e825e57af1c62b8d31723190249910ff5
root <besckinder@gmail.com>
2023-07-27 07:16:00 -0700

    did task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,18 +10,50 @@
 
 int main(int ac, char *av[])
 {
-	int o, r, w;
-	char *content = NULL;
+	int o, p, r, c, d;
+	char buf[BUFSIZ];
 
 	if (ac != 3)
 	{
-		perror("cp");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit (97);
 	}
 
-	o = open(av[0], O_RDONLY,0664);
+	p = open(av[1], O_RDONLY);
+	if (p < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
-	r = read(o, content, sizeof(av[0]));
+	o = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(p, buf, BUFSIZ)) > 0)
+	{
+		if (o < 0 || write(o, buf, r) != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			close(p);
+			exit(99);
+		}
+	}
+	if (r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
-	w = write(
+	c = close(p);
+	d = close(o);
+	if (c < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", p);
+		exit(100);
+	}
+	if (d < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o);
+		exit(100);
+	}
+	return (0);
+}
 
