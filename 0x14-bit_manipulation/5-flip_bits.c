c1af12ed23f371cfe7fd27374c60edd403ac43d1
BENONI ESCKINDER <besckinder@gmail.com>
2023-08-03 07:59:10 +0300

    first commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - Calculates how many flips need to be flipped to get
+ *	       from one number to another
+ * @n: from this unsigned int
+ * @m: to this unsigned int
+ * Return: The number of bits calculated
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int Xor, count = 0;
+
+	Xor = n ^ m;
+	while (Xor != 0)
+	{
+		if ((Xor & 1) == 1)
+			count++;
+		Xor >>= 1;
+	}
+	return (count);
+}
