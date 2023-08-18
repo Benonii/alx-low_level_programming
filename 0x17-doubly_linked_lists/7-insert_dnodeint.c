5bbe78f445cc37f84451959c9aed12d52a51d577
root <besckinder@gmail.com>
2023-08-18 14:28:41 -0700

    Checker fix attempt 2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx,
 		int n)
 {
-	dlistint_t *node, *current;
+	dlistint_t *node = NULL, *current;
 	unsigned int i = 0;
 
 	if (h == NULL)
