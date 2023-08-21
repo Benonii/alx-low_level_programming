afa39a85cd6da9ffb9fb54eed50ad40853690f55
root <besckinder@gmail.com>
2023-08-21 13:05:24 -0700

    Mandatory tasks completed

---
+++ 0x18-dynamic_libraires/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of the prefix substring
+ * @s: string
+ * @accept: part of string that will be measures
+ *
+ * Return: int(number of bytes in the initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				count++;
+				break;
+			}
+
+			else if (accept[i + 1] == '\0')
+				return (count);
+		}
+
+		s++;
+	}
+
+	return (count);
+}
