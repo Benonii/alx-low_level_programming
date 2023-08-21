09aaa67e5434fe34385dca91bbf3ab91491b7fc3
root <besckinder@gmail.com>
2023-08-21 13:05:24 -0700

    Mandatory tasks completed

---
+++ 0x18-dynamic_libraires/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string from src to dest upto n characters
+ * @dest: destination
+ * @src: source
+ * @n: n
+ *
+ * Return: pointer to char(dest)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	while (src[i++] && i < n)
+		dest[i] = src[i];
+
+	return (dest);
+}
