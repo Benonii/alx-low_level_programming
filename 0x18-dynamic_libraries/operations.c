fb0f8cb1e8afbdbdad98ae6af0bb51b816893df2
root <besckinder@gmail.com>
2023-08-21 20:43:25 -0700

    checker fix atttempt 1

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+int sum(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int mod(int a, int b)
+{
+	return(a % b);
+}
