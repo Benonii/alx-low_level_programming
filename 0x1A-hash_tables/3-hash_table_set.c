e5c7d88a464b4e4aaf0127735d376d537b5ddf50
root <besckinder@gmail.com>
2023-09-08 05:11:21 -0700

    checker fix attempt 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node = malloc(sizeof(hash_node_t *));
+	hash_node_t *node = malloc(sizeof(hash_node_t));
 	unsigned long int index = 0;
 
 	if (node == NULL)
