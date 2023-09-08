de3708bf5cb0f23e669b702c0a6942be38b52c42
root <besckinder@gmail.com>
2023-09-08 05:05:30 -0700

    checker fix attempt 1

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,10 +12,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[index])
 		return (ht->array[index]->value);
 	return (NULL);
 }
-
