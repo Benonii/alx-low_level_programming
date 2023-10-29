1fadfdc369fe24d61b054591eed1690ea023789d
root <besckinder@gmail.com>
2023-10-29 08:17:34 -0700

    This was explained to me by https://gist.github.com/frectonz

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,10 +7,22 @@ def island_perimeter(grid):
     ''' This function calculates the perimeter of an island csurrounded by
         water. '''
 
-    land = 0
+    perimeter = 0
+    for i in range(len(grid)):
+        row = grid[i]
+        for j in range(len(row)):
+            cell = grid[i][j]
 
-    for lst in grid:
-        for i in lst:
-            if i == 1:
-                land += 1
-    return (land)* 2 + 2
+            if cell != 1:
+                continue
+
+            top = grid[i - 1][j] if i != 0 else 0
+            bottom = grid[i + 1][j] if i <= len(grid) - 1 else 0
+            left = grid[i][j - 1] if j != 0 else 0
+            right = grid[i][j + 1] if j <= len(grid) - 1 else 0
+
+            for neighbors in [top, bottom, left, right]:
+                if neighbors == 0:
+                    perimeter += 1
+
+    return perimeter
